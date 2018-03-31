ae16be30ad243192ad8d7da28ac5df9c137ba2d2
vikash-kr <iit2016041@iiita.ac.in>
2018-03-31 15:46:10 +0530

    More setup, useless files removed

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require("express");
 var app = express();
 
+// App config
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// Get requests
+
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
+app.get("/query", function (req, res) {
+    res.render("query");
+});
+
+// Server listen request
 app.listen(3000, function () {
     console.log("Server initiated!");
 });
