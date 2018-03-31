d8668722fbe19591c7cc1905c02f07c702d69c56
MaanasVohra <maanasvohra1998@gmail.com>
2018-03-31 18:39:35 +0530

    Login and Cart Page left

--- app.js
+++ app.js
@@ -12,13 +12,34 @@ app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
+// login page
+app.get("/login", function(req, res){
+    res.send("Login");
+});
+
+// sign up
+app.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
 app.get("/query", function (req, res) {
     res.render("query");
 });
 
+// profile page of user/admin
 app.get("/profile", function (req, res) {
     res.render("profile");
-})
+});
+
+// history of payments for user
+app.get("/payments", function(req, res){
+    res.render("payments");
+});
+
+// about page
+app.get("/about", function(req, res) {
+    res.render("about");
+});
 
 // Server listen request
 app.listen(3000, function () {
